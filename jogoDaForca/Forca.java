ff4387cb84b7955e6faebf70d659a6ff9930f256
Túlio, the Duke. <70290204+tuliofh01@users.noreply.github.com>
2026-02-17 11:40:40 -0300

    Delete jogoDaForca directory

--- jogoDaForca/Forca.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Forca  {
-    public static void main(String[] args) {
-        Janela janela = new Janela();
-        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-}
